1119b7844327664a0451c04ed9c940045d26681e
rajeet <mangratirajit@gmail.com>
2020-06-03 10:17:02 +0545

    First Commit

--- src/Province/Province.js
+++ src/Province/Province.js
@@ -60,7 +60,7 @@ function Province() {
                     </div>
                     <div className="card-body">
                         <h5 className="card-title">
-                            {active.count}hello
+                            {}hello
                         </h5>
                         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                     </div>
